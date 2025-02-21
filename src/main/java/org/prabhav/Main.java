524eb00164e7f5ad3f44a80dc754d16a60313434
Prabhav49 <iamprabhav23@gmail.com>
2025-02-21 14:18:53 +0530

    Updated Calculator

--- src/main/java/org/prabhav/Main.java
+++ src/main/java/org/prabhav/Main.java
@@ -3,12 +3,15 @@ package org.prabhav;
 public class Main {
     public static void main(String[] args) {
         ScientificCalculator calculator = new ScientificCalculator();
-        double sq = calculator.squareRoot(25); // Example input
+        double sq = calculator.squareRoot(25);
         System.out.println("Square root of 25 is: " + sq);
+
         long fac = calculator.factorial(25);
         System.out.println("Factorial of 25 is: " + fac);
-        double nl = calculator.factorial(5);
+
+        double nl = calculator.naturalLog(5);
         System.out.println("Natural Log of 5 is: " + nl);
+
         double pow = calculator.power(4, 2);
         System.out.println("Power of 4 ^ 2: " + pow);
     }
