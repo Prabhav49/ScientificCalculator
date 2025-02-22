b2e6ff2e72b82be564d12c9c0fa3f6cf8cb2daff
Prabhav49 <iamprabhav23@gmail.com>
2025-02-22 15:41:22 +0530

    Changed some user input logic

--- src/main/java/org/prabhav/Main.java
+++ src/main/java/org/prabhav/Main.java
@@ -2,31 +2,40 @@ package org.prabhav;
 
 public class Main {
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Error: No input provided. Pass a number as an argument.");
+        if (args.length < 3) {
+            System.out.println("Usage: java org.prabhav.Main <choice> <num1> <num2>");
+            System.out.println("1: Square Root  |  2: Factorial  |  3: Natural Log  |  4: Power");
             return;
         }
 
         int choice = Integer.parseInt(args[0]);
+        double num1 = Double.parseDouble(args[1]);
+        double num2 = Double.parseDouble(args[2]);
+
         ScientificCalculator calculator = new ScientificCalculator();
+        double result = 0;
 
         switch (choice) {
             case 1:
-                double sq = calculator.squareRoot(25);
-                System.out.println("Square root of 25 is: " + sq);
+                result = calculator.squareRoot(num1);
+                System.out.println("Square root of " + num1 + " is: " + result);
                 break;
+
             case 2:
-                long fac = calculator.factorial(5);
-                System.out.println("Factorial of 5 is: " + fac);
+                long fact = calculator.factorial((int) num1);
+                System.out.println("Factorial of " + (int) num1 + " is: " + fact);
                 break;
+
             case 3:
-                double nl = calculator.naturalLog(5);
-                System.out.println("Natural Log of 5 is: " + nl);
+                result = calculator.naturalLog(num1);
+                System.out.println("Natural log of " + num1 + " is: " + result);
                 break;
+
             case 4:
-                double pow = calculator.power(4, 2);
-                System.out.println("Power of 4 ^ 2: " + pow);
+                result = calculator.power(num1, num2);
+                System.out.println(num1 + " raised to the power of " + num2 + " is: " + result);
                 break;
+
             default:
                 System.out.println("Invalid choice!");
         }
